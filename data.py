e6257158c369a415d0a823ab1315b8499deedd9b
Egan OCallaghan <eganocallagham@gmail.com>
2025-09-08 17:15:20 -0700

    Update quiz dict with json data

--- data.py
+++ data.py
@@ -1,18 +1,35 @@
 question_data = [
-    {"text": "A slug's blood is green.", "answer": "True"},
-    {"text": "The loudest animal is the African Elephant.", "answer": "False"},
-    {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
-    {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
     {
-        "text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home "
-                "to eat.",
-        "answer": "True"},
-    {"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.",
-     "answer": "False"},
-    {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
-    {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
-    {"text": "Google was originally called 'Backrub'.", "answer": "True"},
-    {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
-    {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
-    {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
-]
+        "type": "boolean",
+        "difficulty": "medium",
+        "category": "Science: Mathematics",
+        "question": "E = MC3",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Entertainment: Video Games",
+     "question": "In the Monster Hunter Series, G-Rank is the highest possible prestige a Hunter can recieve.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Entertainment: Books",
+     "question": "Dracula is destroyed by contact with sunlight", "correct_answer": "False",
+     "incorrect_answers": ["True"]}, {"type": "boolean", "difficulty": "medium", "category": "Science: Computers",
+                                      "question": "All program codes have to be compiled into an executable file in "
+                                                  "order to be run. This file can then be executed on any machine.",
+                                      "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"type": "boolean", "difficulty": "medium", "category": "Geography",
+     "question": "Antarctica is the largest desert in the world.", "correct_answer": "True",
+     "incorrect_answers": ["False"]}, {"type": "boolean", "difficulty": "medium", "category": "History",
+                                       "question": "The two atomic bombs dropped on Japan by the United States in "
+                                                   "August 1945 were named &#039;Little Man&#039; and &#039;Fat "
+                                                   "Boy&#039;.",
+                                       "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Entertainment: Japanese Anime &amp; Manga",
+     "question": "The anime &quot;Lucky Star&quot; follows the story of one girl who is unaware she is God.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Art",
+     "question": "Leonardo da Vinci&#039;s Mona Lisa does not have eyebrows.", "correct_answer": "True",
+     "incorrect_answers": ["False"]}, {"type": "boolean", "difficulty": "easy", "category": "Geography",
+                                       "question": "There is a city called Rome in every continent on Earth.",
+                                       "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Entertainment: Video Games",
+     "question": "The Kerbol System (from Kerbal Space Program) includes a Saturn analogue.", "correct_answer": "False",
+     "incorrect_answers": ["True"]}]
